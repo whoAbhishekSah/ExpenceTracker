04154603c6de0ccca8f250b1bc540f2ca8735a1d
Abhishek Sah <abhi.sah97@gmail.com>
2024-03-21 21:24:16 +0530

    create basic form view

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
+
+let totalSpent = 0
+
+const ExpenceAmountInputBox = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={{}}>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Amount spent"
+        keyboardType='number-pad'
+        onChangeText={newText => { setText(newText)}}
+        defaultValue={text}
+      />
+    </View>
+  );
+};
+
+const CategoryInputBox = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={{}}>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Categories"
+        maxLength={20}
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+    </View>
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>The quick Brown fox jumped over the lazy dog!</Text>
-      <StatusBar style="auto" />
+      <Text>Expence Tracking</Text>
+      <ExpenceAmountInputBox></ExpenceAmountInputBox>
+      <CategoryInputBox></CategoryInputBox>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert("Total Expence", totalSpent, [
+          {
+            text: 'Revert',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ])}
+      />
     </View>
   );
 }
